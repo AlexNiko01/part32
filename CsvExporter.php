4e49a33a72491402588650d37c53662469b27aa6
Alexandra Semenckuk <a.semenckuk@bintime.com>
2018-04-17 17:52:56 +0300

    csv writing

--- CsvExporter.php
+++ CsvExporter.php
@@ -5,5 +5,15 @@
  */
 class CsvExporter
 {
-
+    /**
+     * @param $productData
+     */
+    public function saveCsv($productData)
+    {
+        $file = 'csv/product_distributors.csv';
+        chmod('csv', 0775);
+        $fp = fopen($file, 'w');
+        fwrite($fp, $productData);
+        fclose($fp);
+    }
 }
--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 ini_set('memory_limit', '64Mb');
 require('FileReader.php');
 require('JsonParser.php');
+require('CsvExporter.php');
 
 $fileReader = new FileReader('txt/product_distributors.txt');
 $generator = $fileReader->readFile();
 $jsonParser = new JsonParser($generator);
 
-$arr = $jsonParser->parseJson();
-var_dump(
-    $arr
-);
+$productData = $jsonParser->parseJson();
+//var_dump(
+//    $productData
+//);
+
+$csvExporter = new CsvExporter();
+$csvExporter->saveCsv($productData);
